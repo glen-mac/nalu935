149f4e23dcf295450c772e6db0be9b02e5f111ee
ㅤ <osxusr@users.noreply.github.com>
2017-03-07 18:50:18 -0500

    Create offsets.c

---
+++ 32 bit only iOS 9.3.5 Jailbreak/offsets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/utsname.h>
+
+#include "offsets.h"
+
+uint64_t allproc_offset;
+uint64_t kernproc_offset;
+uint64_t procoff;
+uint64_t rootvnode_offset;
+
+void init_offsets() {
+    struct utsname u = { 0 };
+    uname(&u);
+
+    printf("sysname: %s\n", u.sysname);
+    printf("nodename: %s\n", u.nodename);
+    printf("release: %s\n", u.release);
+    printf("version: %s\n", u.version);
+    printf("machine: %s\n", u.machine);
+
+    allproc_offset = 0x;
+    procoff = 0x;
+    rootvnode_offset = 0x;
+
+    /*
+    Offsets shared by:
+    - @iosusr
+    */
+
+    if (strcmp(u.version, "Darwin Kernel Version 16.3.0: Tue Nov 29 21:40:09 PST 2016; root:xnu-3789.32.1~4/RELEASE_ARM64_") == 0) {
+        allproc_offset = 0x;
+        rootvnode_offset = 0x;
+    }
