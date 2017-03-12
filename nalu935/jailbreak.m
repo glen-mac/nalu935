25988b78a1d45200358574a69e119f3031ede08a
ㅤ <osxusr@users.noreply.github.com>
2017-03-11 23:09:07 -0500

    Update jailbreak.m

--- nalu935/jailbreak.m
+++ nalu935/jailbreak.m
@@ -1,1 +1,19 @@
-j
+//
+//  ViewController.m
+//  nalu935
+//
+//  Created by @iosusr
+//  Copyright © 2017 @iosusr. All rights reserved.
+//
+
+#undef __IPHONE_OS_VERSION_MIN_REQUIRED
+#import "devicesupport.h"
+
+#import <IOKit/IOKitLib.h>
+#import <UIKit/UIKit.h>
+
+#import "devicesupport.h"
+
+
+#import "patchfinder32.h"
+
