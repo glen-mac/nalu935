cba7dcc7a0fd730e8c050af05bf18f78d56ca997
ㅤ <osxusr@users.noreply.github.com>
2017-03-11 22:28:42 -0500

    Update AppDelegate.h

--- 32 bit only iOS 9.3.5 Jailbreak/AppDelegate.h
+++ 32 bit only iOS 9.3.5 Jailbreak/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  32 bit only iOS 9.3.5 Jailbreak
+//  nalu935
 //
-//  Created by Brandon Withall on 2017-02-14.
-//  Copyright © 2017 Brando1235703. All rights reserved.
+//  Created by @iosusr
+//  Copyright © 2017 @iosusr. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
